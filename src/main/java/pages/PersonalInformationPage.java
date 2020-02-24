d0b639ae8d863a59d3c998b9b781635cde8bcf8d
Galiya Khalikova <xalgalrin@yandex.ru>
2020-02-24 23:48:26 +0700

    add 2 tests

--- src/main/java/pages/PersonalInformationPage.java
+++ src/main/java/pages/PersonalInformationPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import java.util.List;
+import org.junit.*;
 
 import static pages.MainPage.inputText_xpath;
 import static pages.MainPage.waitCss;
@@ -12,38 +13,49 @@ import static pages.MainPage.waitCss;
 public class PersonalInformationPage{
     private static WebDriver driver;
 
-    public final static String text = "Personal Information";
-    public final static String personal_inf_css = "[class='h2 step_page__title']";
-    public final static String doc_id = "//*[@id='application_document_number']";
-    public final static String dateofbirth_id = "//*[@id='application_date_of_birth']";
-    public final static String email_id = "//*[@id='application_email']";
-    public final static String button_continue_xpath = "//*[text() ='Continue']";
-    public final static String error_mesg = "//*[contains(text(),'This field is required')]";
+    private final static String text_pi = "Personal Information";
+    private final static String personal_inf_css = "[class='h2 step_page__title']";
+    private final static String doc_id = "//*[@id='application_document_number']";
+    private final static String dateofbirth_id = "//*[@id='application_date_of_birth']";
+    private final static String email_id = "//*[@id='application_email']";
+    private final static String button_continue_xpath = "//*[text() ='Continue']";
+    private final static String error_mesg = "//*[contains(text(),'This field is required')]";
     private final static String input_doc_id = "1111111111";
+    private final static String input_doc_id_incorr = "111111111";
+    private final static String gender_female_id = "(//*[@class='radio'])[2]";
     private final static String input_dateofbirth = "02151991";
+    private final static String input_dateofbirth_incorr = "02152020";
     private final static String input_email = "xalgalrin@mailinator.com";
-
+    public final static String error_doc_id_mesg = "Please enter valid ID number";
+    private final static String error_doc_id_css = "[class='form__error form__error-static']";
+    public final static String error_dob_mesg = "Invalid Date";
+    private final static String error_dob_css = "[class='form__error']";
 
     public PersonalInformationPage(WebDriver driver) {
 
         this.driver = driver;
     }
 
-    public String checkText(){
+    public void checkText(){
         waitCss(personal_inf_css);
         String text = driver.findElement(By.cssSelector(personal_inf_css)).getText();
-        return text;
+        Assert.assertEquals(text_pi,text);
     }
 
-    private static void selectDocTypeAndInputDocId(){
+    private static void selectDocTypeAndInputDocId(int id){
         Select select = new Select(driver.findElement(By.xpath("//*[@id='application_type_of_document']")));
         select.selectByIndex(1);
-        inputText_xpath(doc_id, input_doc_id);
+        if(id == 1)
+        {
+            inputText_xpath(doc_id, input_doc_id);
+        }
+        else
+            inputText_xpath(doc_id, input_doc_id_incorr);
     }
 
     public static int checkRequiredThreeFields(){
 
-        selectDocTypeAndInputDocId();
+        selectDocTypeAndInputDocId(1);
         driver.findElement(By.xpath(button_continue_xpath)).click();
         List<WebElement> number_errors = driver.findElements(By.xpath(error_mesg));
         int size_list = number_errors.size();
@@ -52,7 +64,7 @@ public class PersonalInformationPage{
 
     public static int checkRequiredTwoFields(){
 
-        selectDocTypeAndInputDocId();
+        selectDocTypeAndInputDocId(1);
         inputText_xpath(dateofbirth_id, input_dateofbirth);
         driver.findElement(By.xpath(button_continue_xpath)).click();
         List<WebElement> number_errors = driver.findElements(By.xpath(error_mesg));
@@ -62,12 +74,34 @@ public class PersonalInformationPage{
 
     public static int checkRequiredOneFields(){
 
-        selectDocTypeAndInputDocId();
+        selectDocTypeAndInputDocId(1);
         inputText_xpath(dateofbirth_id, input_dateofbirth);
         inputText_xpath(email_id, input_email);
         driver.findElement(By.xpath(button_continue_xpath)).click();
         List<WebElement> number_errors = driver.findElements(By.xpath(error_mesg));
         int size_list = number_errors.size();
         return size_list;
+    }
+
+    public static String checkDocIdField(){
+
+        selectDocTypeAndInputDocId(0);
+        driver.findElement(By.xpath(gender_female_id)).click();
+        inputText_xpath(dateofbirth_id, input_dateofbirth);
+        inputText_xpath(email_id, input_email);
+        driver.findElement(By.xpath(button_continue_xpath)).click();
+        String text = driver.findElement(By.cssSelector(error_doc_id_css)).getText();
+        return text;
+    }
+
+    public static String checkDateOfBirthField(){
+
+        selectDocTypeAndInputDocId(1);
+        driver.findElement(By.xpath(gender_female_id)).click();
+        inputText_xpath(dateofbirth_id, input_dateofbirth_incorr);
+        inputText_xpath(email_id, input_email);
+        driver.findElement(By.xpath(button_continue_xpath)).click();
+        String text = driver.findElement(By.cssSelector(error_dob_css)).getText();
+        return text;
     }
 }
--- src/test/java/tests.java
+++ src/test/java/tests.java
@@ -1,53 +1,61 @@
 import org.junit.*;
-import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.chrome.ChromeDriver;
 import pages.MainPage;
 import pages.PersonalInformationPage;
 import pages.StartPage;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
+
 public class tests {
-
-    private static WebDriver driver;
-
-    @BeforeClass
-    public static void setupAll() {
-        driver = Driver.getChromeDriver();
-        driver.manage().timeouts().implicitlyWait(20, SECONDS);
-        driver.manage().window().maximize();
-    }
+    ChromeDriver driver;
 
     @Before
     public void setup() {
+
+        System.setProperty("WebDriver.chrome.driver", "/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, SECONDS);
+        driver.manage().window().maximize();
         MainPage mainPage = new MainPage(driver);
         mainPage.open();
-        StartPage createStartPage = new StartPage(driver);
-        createStartPage.populateStartForm();
+        StartPage.populateStartForm();
         PersonalInformationPage personalInformationPage = new PersonalInformationPage(driver);
-        Assert.assertEquals(PersonalInformationPage.text, personalInformationPage.checkText());
+        personalInformationPage.checkText();
     }
 
     @Test
-    public void check_form_negative_3_fields_are_not_populated() {
-        Assert.assertEquals(2,PersonalInformationPage.checkRequiredThreeFields());
+    public void three_fields_are_not_populated() {
+        Assert.assertEquals(3,PersonalInformationPage.checkRequiredThreeFields());
         System.out.println("Test 1 is passed");
     }
 
     @Test
-    public void check_form_negative_2_fields_are_not_populated() {
+    public void two_fields_are_not_populated() {
         Assert.assertEquals(2,PersonalInformationPage.checkRequiredTwoFields());
         System.out.println("Test 2 is passed");
     }
 
     @Test
-    public void check_form_negative_1_field_is_not_populated() {
+    public void one_field_is_not_populated() {
         Assert.assertEquals(1,PersonalInformationPage.checkRequiredOneFields());
         System.out.println("Test 3 is passed");
     }
 
-    @AfterClass
-    public static void quit() {
+    @Test
+    public void validate_field_doc_id() {
+        Assert.assertEquals(PersonalInformationPage.error_doc_id_mesg,PersonalInformationPage.checkDocIdField());
+        System.out.println("Test 4 is passed");
+    }
+
+    @Test
+    public void validate_field_date_of_birth() {
+        Assert.assertEquals(PersonalInformationPage.error_dob_mesg,PersonalInformationPage.checkDateOfBirthField());
+        System.out.println("Test 5 is passed");
+    }
+
+    @After
+    public void quit() {
         driver.quit();
     }
 
